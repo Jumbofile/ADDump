d21274f277128c877daff03b067424609493fb51
Gregory Plachno <gplachno@ycp.edu>
2017-07-06 16:46:30 -0400

    PowerShell Script

--- WindowsFormsApp1/adcomputertocsv.ps1
+++ WindowsFormsApp1/adcomputertocsv.ps1
@@ -11,7 +11,6 @@ ______________________________________________________________#>
 
     #Import Active Directory and dump Computers info into a excel sheet
     Import-Module ActiveDirectory
-    #Param($path)
 
     #User Properties that are going to be listed
     Get-ADComputer -Filter * -Properties * |
