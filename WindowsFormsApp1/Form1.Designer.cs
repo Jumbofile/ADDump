c603b233dde696f7a700964c4c5e3a02622a2317
Gregory Plachno <GPlachno@dymax.com>
2017-07-06 14:48:14 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ActiveDirectoryDump
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runButton = new System.Windows.Forms.Button();
+            this.folderBox = new System.Windows.Forms.TextBox();
+            this.folderButton = new System.Windows.Forms.Button();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scriptLocationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.byGregPlachnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.v110ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runtimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.installToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(97, 68);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(109, 56);
+            this.runButton.TabIndex = 0;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBox
+            // 
+            this.folderBox.Location = new System.Drawing.Point(11, 42);
+            this.folderBox.Name = "folderBox";
+            this.folderBox.ReadOnly = true;
+            this.folderBox.Size = new System.Drawing.Size(228, 20);
+            this.folderBox.TabIndex = 1;
+            this.folderBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // folderButton
+            // 
+            this.folderButton.Location = new System.Drawing.Point(245, 42);
+            this.folderButton.Name = "folderButton";
+            this.folderButton.Size = new System.Drawing.Size(48, 21);
+            this.folderButton.TabIndex = 2;
+            this.folderButton.Text = "Save";
+            this.folderButton.UseVisualStyleBackColor = true;
+            this.folderButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(12, 130);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(281, 119);
+            this.richTextBox.TabIndex = 3;
+            this.richTextBox.Text = "";
+            this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Save Location";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(305, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.scriptLocationToolStripMenuItem,
+            this.runtimeToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.byGregPlachnoToolStripMenuItem,
+            this.v110ToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // scriptLocationToolStripMenuItem
+            // 
+            this.scriptLocationToolStripMenuItem.Name = "scriptLocationToolStripMenuItem";
+            this.scriptLocationToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.scriptLocationToolStripMenuItem.Text = "Script";
+            this.scriptLocationToolStripMenuItem.Click += new System.EventHandler(this.scriptLocationToolStripMenuItem_Click);
+            // 
+            // byGregPlachnoToolStripMenuItem
+            // 
+            this.byGregPlachnoToolStripMenuItem.Name = "byGregPlachnoToolStripMenuItem";
+            this.byGregPlachnoToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.byGregPlachnoToolStripMenuItem.Text = "By: Greg Plachno";
+            // 
+            // v110ToolStripMenuItem
+            // 
+            this.v110ToolStripMenuItem.Name = "v110ToolStripMenuItem";
+            this.v110ToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.v110ToolStripMenuItem.Text = "v1.1.0";
+            // 
+            // runtimeToolStripMenuItem
+            // 
+            this.runtimeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.installToolStripMenuItem});
+            this.runtimeToolStripMenuItem.Name = "runtimeToolStripMenuItem";
+            this.runtimeToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.runtimeToolStripMenuItem.Text = "Runtime";
+            // 
+            // installToolStripMenuItem
+            // 
+            this.installToolStripMenuItem.Name = "installToolStripMenuItem";
+            this.installToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.installToolStripMenuItem.Text = "Install";
+            this.installToolStripMenuItem.Click += new System.EventHandler(this.installToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 258);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.folderButton);
+            this.Controls.Add(this.folderBox);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Active Directory Dump";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox folderBox;
+        private System.Windows.Forms.Button folderButton;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scriptLocationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem byGregPlachnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem v110ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runtimeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem installToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+/*Active Directory Dump
+ * By: Gregory Plachno
+ * Made: 7/6/2017
+ * v1.1.0
+ */
+namespace ActiveDirectoryDump
+{
+    public partial class Form1 : Form
+    {
+        public String FilePathVariable = "";
+        public String scriptPath = "";
+        public String runtimePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Setting output folder
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    //Setting output folder as a variable
+                    FilePathVariable = fbd.SelectedPath;
+                    folderBox.Text = FilePathVariable;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox.AppendText(Environment.NewLine + "Start.");
+            // run our script and put the result into our textbox 
+            // NOTE: make sure to change the path to the correct location of your script 
+            if (scriptPath != "")
+            {
+                //run
+                folderBox.Text = RunScript(LoadScript(@scriptPath));
+            }
+            else
+            {
+                //Setting script location
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "PowerShell Scripts (*.ps1)|*.ps1";
+                openFileDialog1.FilterIndex = 0;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    scriptPath = openFileDialog1.FileName;
+                    richTextBox.AppendText(Environment.NewLine + "Script Location Updated.");
+                }
+
+                //run
+                folderBox.Text = RunScript(LoadScript(@scriptPath));
+            }
+            //Rich text append
+            richTextBox.AppendText(Environment.NewLine + "Done.");
+            richTextBox.AppendText(Environment.NewLine + "File located at " + FilePathVariable);
+        }
+
+        private string RunScript(string scriptText)
+        {
+            // create Powershell runspace 
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            richTextBox.AppendText(Environment.NewLine + "Script Started.");
+            // open it 
+            runspace.Open();
+
+            // create a pipeline and feed it the script text 
+            Pipeline pipeline = runspace.CreatePipeline();
+            pipeline.Commands.AddScript(scriptText);
+
+            // add an extra command to transform the script output objects into nicely formatted strings 
+            // remove this line to get the actual objects that the script returns. For example, the script 
+            // "Get-Process" returns a collection of System.Diagnostics.Process instances. 
+            pipeline.Commands.Add("Out-String");
+            runspace.SessionStateProxy.SetVariable("path", FilePathVariable);
+            // execute the script 
+            Collection<PSObject> results = pipeline.Invoke();
+
+            // close the runspace 
+            runspace.Close();
+            richTextBox.AppendText(Environment.NewLine + "Script Compelted.");
+            // convert the script result into a single string 
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+
+            // return the results of the script that has 
+            // now been converted to text 
+            return stringBuilder.ToString();
+        }
+
+        // helper method that takes your script path, loads up the script 
+        // into a variable, and passes the variable to the RunScript method 
+        // that will then execute the contents 
+        private string LoadScript(string filename)
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from our file. 
+                // The using statement also closes the StreamReader. 
+                using (StreamReader sr = new StreamReader(filename))
+                {
+
+                    // use a string builder to get all our lines from the file 
+                    StringBuilder fileContents = new StringBuilder();
+
+                    // string to hold the current line 
+                    string curLine;
+
+                    // loop through our file and read each line into our 
+                    // stringbuilder as we go along 
+                    while ((curLine = sr.ReadLine()) != null)
+                    {
+                        // read each line and MAKE SURE YOU ADD BACK THE 
+                        // LINEFEED THAT IT THE ReadLine() METHOD STRIPS OFF 
+                        fileContents.Append(curLine + "\n");
+                    }
+
+                    // call RunScript and pass in our file contents 
+                    // converted to a string 
+                    return fileContents.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong. 
+                string errorText = "The file could not be read:";
+                errorText += e.Message + "\n";
+                return errorText;
+            }
+            richTextBox.AppendText(Environment.NewLine + "Script Loaded.");
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scriptLocationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Setting script location
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "PowerShell Scripts (*.ps1)|*.ps1";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                scriptPath = openFileDialog1.FileName;
+                richTextBox.AppendText(Environment.NewLine + "Script Location Updated.");
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void installToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //download the runtime files
+            richTextBox.AppendText(Environment.NewLine + "Downloading runtime.");
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://download.microsoft.com/download/1/D/8/1D8B5022-5477-4B9A-8104-6A71FF9D98AB/WindowsTH-RSAT_WS2016-x64.msu", @"c:/WindowsTH-RSAT_WS2016-x64.msu");
+            }
+            richTextBox.AppendText(Environment.NewLine + "Download Complete.");
+
+            //open runtime install
+            richTextBox.AppendText(Environment.NewLine + "Installing Runtime.");
+            System.Diagnostics.Process.Start("c:/WindowsTH-RSAT_WS2016-x64.msu");
+            richTextBox.AppendText(Environment.NewLine + "Install Complete.");
+        }
+
+    }
+}
